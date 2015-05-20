3a74c1c85500f2b20311ea7a9007f0db748fe74f
Ralph Sliger <rsliger19@gmail.com>
2015-05-20 16:07:33 -0500

    Update Revistas.cs

--- AppGestionEditorial/Modelo/Revistas.cs
+++ AppGestionEditorial/Modelo/Revistas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +7,37 @@ namespace AppGestionEditorial.Modelo
 {
     public class Revistas : Documentos
     {
-        public Revistas():base() { }
-        public Revistas(string autor, string NombreDocumento, string genero, int año, string idiomaDocumento) : base(autor, NombreDocumento, genero, año, idiomaDocumento) { }
+        #region "Atributos"
+        private int numeroVolumen;
+
+     
+        private int numeroArticulos;
+
+       
+#endregion
+#region "propiedades"
+        public int NumeroVolumen
+        {
+            get { return numeroVolumen; }
+            set { numeroVolumen = value; }
+        }
+
+        public int NumeroArticulos
+        {
+            get { return numeroArticulos; }
+            set { numeroArticulos = value; }
+        }
+
+
+#endregion
+
+      #region metodos
+      #endregion
+
+
+      public Revistas():base() { }
+        public Revistas(string codigo, string titulo, string autor,  string idioma , string editorial , string categoria , int numeroDePaginas) : base(codigo,titulo,autor,idioma,editorial,categoria,numeroDePaginas){}
+         
 
     }
 }
