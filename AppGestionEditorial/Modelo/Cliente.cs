b9a58cc294be414056ae10c3b9ec2d7648e69f35
ralphsliger <rsliger19@gmail.com>
2015-05-26 06:55:56 -0500

    commit

---
+++ AppGestionEditorial/Modelo/Cliente.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEditorial.Modelo
+{
+    public class Cliente
+    {
+
+        #region "__________Atributos________________________"
+
+        private int id;
+        private string nombres;
+        private string apellidos;
+        private string direccion;  
+        private int telefono;
+        private string email;
+
+        #endregion
+
+        #region"______________ propiedades__________________"
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombres
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        public int Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        #endregion
+
+
+        #region Constructores
+        public Cliente() {
+
+            this.id = 2;
+            this.nombres = "Ralph ";
+            this.apellidos = "Sliger Ordoñez";
+            this.direccion = "los alpes edif Villa sofia";
+            this.telefono = 44444;
+            this.email = "R@gmail.com";
+        
+        }
+
+        public Cliente(int id,string nombres ,string apellidos,string direccion,int telefono,string email){
+
+            this.id = id;
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.email = email;
+
+        }
+        #endregion
+
+
+        #region "Métodos SobreEscritos"
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                               "\n Id " + this.id +
+                               "\n Nombres " + this.nombres +
+                               "\n Apellidos " + this.apellidos +
+                               "\n Direccion " + this.direccion +
+                               "\n Telefono " + this.telefono +
+                               "\n Email " + this.email +
+                               "\n_______________________________________";
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            Cliente o = (Cliente)obj;
+            bool result = false;
+
+            if (base.Equals(o) && (this.id == o.id)
+                && (this.nombres == o.nombres)
+                && (this.apellidos == o.apellidos)
+                && (this.direccion == o.direccion)
+                && (this.telefono == o.telefono)
+                && (this.email == o.email)
+                )
+
+                result = true;
+
+            return result;
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ AppGestionEditorial/Modelo/Formato.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEditorial.Modelo
+{
+    public abstract class Formato
+    {
+
+        #region "____________Atributos_________"
+
+        private int codigo;
+        private string nombre;
+        private bool leer;
+        private bool guardar;
+        private bool imprimir;
+        private bool editar;
+        private bool escuchar;
+        private bool visualizar;
+
+        #endregion
+
+        #region"_______________ Propiedades_____________________"
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public bool Leer
+        {
+            get { return leer; }
+            set { leer = value; }
+        }
+
+        public bool Guardar
+        {
+            get { return guardar; }
+            set { guardar = value; }
+        }
+        public bool Imprimir
+        {
+            get { return imprimir; }
+            set { imprimir = value; }
+        }
+        public bool Editar
+        {
+            get { return editar; }
+            set { editar = value; }
+        }
+        public bool Escuchar
+        {
+            get { return escuchar; }
+            set { escuchar = value; }
+        }
+
+        public bool Visualizar
+        {
+            get { return visualizar; }
+            set { visualizar = value; }
+        }
+
+        #endregion
+
+        #region "____CONSTRUCTORES________"
+        public Formato() {
+
+            this.codigo =1234;
+            this.nombre = "nombre";
+            this.leer = true;
+            this.guardar = true;
+            this.imprimir = true;
+            this.editar = true;
+            this.escuchar=true;
+            this.escuchar = true;
+            this.visualizar = true;
+
+
+        }
+        public Formato(int codigo,string nombre,bool leer,bool guardar,bool imprimir ,bool editar,bool escuchar,bool visualizar) {
+
+            this.codigo = codigo;
+            this.nombre=nombre;
+            this.leer = leer;
+            this.guardar = guardar;
+            this.imprimir = imprimir;
+            this.editar = editar;
+            this.escuchar = escuchar;
+            this.visualizar = visualizar;
+        }
+
+
+        #endregion
+
+        #region "Métodos SobreEscritos"
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                               "\n Id " + this.codigo +
+                               "\n Nombres " + this.nombre +
+                               "\n Apellidos " + this.leer +
+                               "\n Direccion " + this.guardar +
+                               "\n Telefono " + this.imprimir +
+                               "\n Email " + this.editar +
+                                "\n Email " + this.escuchar +
+                                 "\n Email " + this.visualizar +
+                               "\n_______________________________________";
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            Formato o = (Formato)obj;
+            bool result = false;
+
+            if (base.Equals(o) && (this.codigo == o.codigo)
+                && (this.nombre == o.nombre)
+                && (this.leer == o.leer)
+                && (this.guardar == o.guardar)
+                && (this.imprimir == o.imprimir)
+                && (this.editar == o.editar)
+                && (this.escuchar == o.escuchar)
+                && (this.visualizar == o.visualizar)
+                
+                )
+
+                result = true;
+
+            return result;
+        }
+        #endregion
+
+    }
+}
